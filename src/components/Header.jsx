59d70375990c5f8d13455e327f6ac62e6b489494
Julio Cesar <jccintr@gmail.com>
2025-08-24 23:04:03 -0300

    alterado tm fonte navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,18 @@ import logo from '../assets/logo-600x120.png';
 const Header = () => {
   return (
     <Navbar 
-      className='w-full py-4 border-b-2 md:px-20'
+      className='w-full py-4 border-b-2 md:px-10 lg:px-20'
       theme={navbarCustomTheme}>
-      <Navbar.Brand  href="https://jc-software.com">
+      <Navbar.Brand href="https://jc-software.com">
          <img src={logo} alt='logo' className='w-52 sm:w-60 md:w-80' />
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
 
-        <Navbar.Link className='font-semibold text-lg uppercase text-blue-800' href="#servicos">Serviços</Navbar.Link>
-        <Navbar.Link className='font-semibold text-lg uppercase text-blue-800' href="#tecnologias">Tecnologias</Navbar.Link>
-        <Navbar.Link className='font-semibold text-lg uppercase text-blue-800' href="#portfolio">Portfólio</Navbar.Link>
-        <Navbar.Link className='font-semibold text-lg uppercase text-blue-800' href="#contato">Fale Conosco</Navbar.Link>
+        <Navbar.Link className='font-semibold uppercase text-blue-800' href="#servicos">Serviços</Navbar.Link>
+        <Navbar.Link className='font-semibold  uppercase text-blue-800' href="#tecnologias">Tecnologias</Navbar.Link>
+        <Navbar.Link className='font-semibold  uppercase text-blue-800' href="#portfolio">Portfólio</Navbar.Link>
+        <Navbar.Link className='font-semibold  uppercase text-blue-800' href="#contato">Fale Conosco</Navbar.Link>
        
       </Navbar.Collapse>
     </Navbar>
